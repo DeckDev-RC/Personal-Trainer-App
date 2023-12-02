4c1f16b28876452ef5ef5d29a37be24ba3a0fe07
Renato Deck-Dev <deeckinve@proton.me>
2023-12-02 00:04:55 -0300

    create main, login page and components and add some assets images

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:workout_app/components/button_login.dart';
+import 'package:workout_app/components/my_text_field.dart';
+import 'package:workout_app/components/square_tile.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,28 +11,99 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue[600],
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              children: [
-                SizedBox(height: 80),
-                Icon(
-                  Icons.fitness_center,
-                  color: Colors.black,
-                  size: 100,
-                ),
-                SizedBox(height: 30),
-                Text(
-                  'Personal Trainer App',
-                  style: TextStyle(fontSize: 30),
-                )
-              ],
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 25),
+                  CircleAvatar(
+                    backgroundColor: Colors.grey[700],
+                    radius: 60,
+                    child: const Icon(
+                      Icons.fitness_center_outlined,
+                      size: 100,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'F I T N E S S  A P P',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 25),
+                  MyTextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false,
+                  ),
+                  const SizedBox(height: 15),
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Senha',
+                    obscureText: true,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Esqueceu a Senha?',
+                          style: TextStyle(
+                            color: Colors.grey[200],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  ButtonLogin(
+                    onTap: () {},
+                    text: 'Entrar',
+                  ),
+                  const SizedBox(height: 15),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Acesse as nossas redes sociais',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Divider(
+                    thickness: 0.5,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 10),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SquareTile(imagePath: 'assets/images/instagram.png'),
+                      SizedBox(width: 20),
+                      SquareTile(imagePath: 'assets/images/whatsapp.png'),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
